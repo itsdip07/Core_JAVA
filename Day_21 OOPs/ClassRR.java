eb064971fdd3ca582fcc27e5c740d87bd2f10b55
itsdip07 <maitidipayan07@gmail.com>
2026-06-03 21:53:08 +0530

    Added Day_21

---
+++ Day_21 OOPs/ClassRR.java
@@ -0,0 +1,23 @@
+
+/* It is a component of java which has like a blueprint of the object 
+   in order to creat an object it is mandatory to create a class  */
+
+// Let's take an example of Rolls Royce (Car)
+
+class ClassRR {
+   double price;
+   String color;
+   String model;
+   int EngineCC;
+
+   public void AC(){
+
+   }
+   public void carStart(){
+
+   }
+
+   public void music(){
+
+   }
+}
---
+++ Day_21 OOPs/The_Object.java
@@ -0,0 +1,10 @@
+/*
+    OBJECT : It is a real world entity which have some attribute and beheviour
+        In java object is an Instanc of class 
+*/
+
+class The_Object {
+    public static void main(String[] args) {
+        //class name identifires = new className();
+    }
+}
